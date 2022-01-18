746ba2b7b24fca2c8ede2a2cf5b749393abbb4c0
ggroshansii <garthgroshansii@gmail.com>
2022-01-18 10:43:14 -0500

    added data to vue options object

--- first/app.js
+++ first/app.js
@@ -1,3 +1,13 @@
 const app = new Vue({
     el: '#app',
+    data: {
+        username: "Garth",
+        newTweet: "",
+        tweets: [
+            "Learning Vue Today!",
+            "Cannot wait to get started",
+            "Good Morning everyone"
+        ],
+        bio: "Software Apprentice"
+    }
 })
