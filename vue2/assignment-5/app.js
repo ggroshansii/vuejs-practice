e1bfc521663319a602c7a65d333ae61bd3cbf5f3
ggroshansii <garthgroshansii@gmail.com>
2022-01-26 09:07:17 -0500

    completed vue-for topic and assignment 5

---
+++ vue2/assignment-5/app.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    isVisible: false,
+    myArray: ['book1', 'book2', 'book3', 'book4'],
+    array: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT', 
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+});
