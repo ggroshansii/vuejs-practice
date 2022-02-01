cfc8a2b229a4230357e617667561396dc1b68b16
ggroshansii <garthgroshansii@gmail.com>
2022-02-01 09:36:47 -0500

    created event bus to pass data between sibling components

--- vue2/vue-components/passing_data/src/components/UserDetail.vue
+++ vue2/vue-components/passing_data/src/components/UserDetail.vue
@@ -2,13 +2,20 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Username: {{name}}</p>
+        <p>Age: {{userAge}}</p>
         <p>Many Details</p>
         <button @click="changeAgain">Change Again</button>
     </div>
 </template>
 
 <script>
+import { eventBus } from '../main';
     export default {
+        data: function() {
+            return {
+                userAge: 35,
+        }
+        },
         props: { 
             name: String,
         },
@@ -17,6 +24,11 @@
                 this.name = "Max"
                 this.$emit("nameChanged", this.name)
             }
+        },
+        created() {
+            eventBus.$on('changedAge', (data) => {
+                this.userAge = data;
+            })
         }
     }
 </script>
--- vue2/vue-components/passing_data/src/components/UserEdit.vue
+++ vue2/vue-components/passing_data/src/components/UserEdit.vue
@@ -2,10 +2,26 @@
     <div class="component">
         <h3>You may edit the User here</h3>
         <p>Edit me!</p>
+        <p>Age: {{userAge}}</p>
+        <button @click="changeTo40">Change Age to 40</button>
     </div>
 </template>
 
 <script>
+import { eventBus } from '../main';
+export default {
+    data: function() {
+        return {
+            userAge: 30
+        }
+    },
+    methods: {
+        changeTo40() {
+            this.userAge = 40;
+            eventBus.$emit("changedAge", this.userAge);
+        }
+    }
+}
 </script>
 
 <style scoped>
