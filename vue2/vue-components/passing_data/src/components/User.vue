fe6c93ebd3d260abc7dee3a13f425f9c01a1f976
ggroshansii <garthgroshansii@gmail.com>
2022-01-31 23:01:26 -0500

    passing data p->c and c->p

---
+++ vue2/vue-components/passing_data/src/components/User.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change Me</button>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail :name="firstName" @nameChanged="firstName=$event" ></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  data:
+    function() {
+      return {
+        firstName: "Garth",
+      };
+    },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit,
+  },
+  methods: {
+      changeName() {
+          this.firstName = "Anna"
+      }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
---
+++ vue2/vue-components/passing_data/src/components/UserDetail.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Username: {{name}}</p>
+        <p>Many Details</p>
+        <button @click="changeAgain">Change Again</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: { 
+            name: String,
+        },
+        methods: {
+            changeAgain() {
+                this.name = "Max"
+                this.$emit("nameChanged", this.name)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
